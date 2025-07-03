cbf3a0bc17d8a700be0571b37fab375af5ca3e91
Nikesh Kumar <nikesh23kumar@gmail.com>
2025-07-03 22:48:18 +0530

    updated android emulator file

--- src/test/java/automation/InstallAppAndroidEmulator.java
+++ src/test/java/automation/InstallAppAndroidEmulator.java
@@ -2,7 +2,7 @@ package automation;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-
+import java.time.Duration;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -13,20 +13,19 @@ public class InstallAppAndroidEmulator {
 	public static void main(String[] args) throws MalformedURLException {
 
 		DesiredCapabilities dc = new DesiredCapabilities();
-		
+
 		dc.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
 		dc.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-		dc.setCapability(MobileCapabilityType.PLATFORM_VERSION, "13.0"); 
-		dc.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator"); 
+		dc.setCapability(MobileCapabilityType.PLATFORM_VERSION, "13.0");
+		dc.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
 		dc.setCapability(MobileCapabilityType.APP, "C:\\apkfiles\\IndiaMart.apk");
 		dc.setCapability(MobileCapabilityType.NO_RESET, true);
 
 		// Start session with Appium server (running on localhost:4723)
 		URL url = new URL("http://127.0.0.1:4723/wd/hub");
 		AndroidDriver<WebElement> driver = new AndroidDriver<>(url, dc);
-
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		System.out.println("App Launched Successfully!");
-		
 
 		driver.quit();
 	}
